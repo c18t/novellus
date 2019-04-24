8ada59cc1ab2de785ff9c3ca2a5283122c3d7c0f
鵜巷 <u@chimata.org>
2019-04-25 00:57:25 +0900

    create Xamarin projects and a common project

---
+++ Novellus.Form/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Novellus.Services;
+using Novellus.Views;
+
+namespace Novellus
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
