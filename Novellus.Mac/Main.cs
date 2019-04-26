fce4a6530b27f2a66ce6bb07c55c88f4d5e6782d
鵜巷 <u@chimata.org>
2019-04-26 19:48:52 +0900

    create Xamarin.Mac project

---
+++ Novellus.Mac/Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace Novellus.Mac
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.Main(args);
+        }
+    }
+}
