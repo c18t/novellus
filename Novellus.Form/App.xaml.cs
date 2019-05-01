dbdd28b5f65fd6cb7ab75fe5e59792f89c4198dd
鵜巷 <u@chimata.org>
2019-05-02 00:29:34 +0900

    remove default elements of template and add webview

--- Novellus.Form/App.xaml.cs
+++ Novellus.Form/App.xaml.cs
@@ -13,7 +13,7 @@ namespace Novellus
         {
             InitializeComponent();
 
-            DependencyService.Register<MockDataStore>();
+            //DependencyService.Register<MockDataStore>();
             MainPage = new MainPage();
         }
 
