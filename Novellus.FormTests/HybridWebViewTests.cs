5947bf4e21693a0de1fa0cd22c379e2d4bef990b
鵜巷 <u@chimata.org>
2019-10-15 22:46:29 +0900

    add Novellus.FormsTest project

---
+++ Novellus.FormTests/HybridWebViewTests.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Novellus;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Novellus.Tests
+{
+    [TestClass]
+    public class HybridWebViewTests
+    {
+        [TestMethod]
+        public void GenerateFunctionScriptTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void InjectJavascriptAsyncTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void GetRegisteredActionNamesTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void RegisterActionTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void RemoveActionTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void RemoveAllActionsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void HandleScriptReceivedTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void DisposeTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
